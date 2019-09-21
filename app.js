2435e2a17dd45c6016a81569499d368cc217e244
Student <student@itpark.ru>
2019-09-21 11:09:09 +0300

    Рефакторинг: избавились от else

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 console.log('worked');
 
 const purchaseAmount = 1000;
-const bonuses = 900;
+const bonuses = 100;
 
-let total;
-if (purchaseAmount <= bonuses) {
+let total = purchaseAmount - bonuses;
+if (total < 0) {
     total = 0;
-} else { 
-    total = purchaseAmount - bonuses;
 }
-
 
 console.log(total);
